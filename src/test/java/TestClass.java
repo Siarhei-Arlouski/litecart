696cbc91f78ba34425a6662ac348ad10d30e846b
Siarhei Arlouski <siarhei.arlouski@gmail.com>
2023-01-14 21:43:47 +0300

    Added features and descriptions

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.ElementsCollection;
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.RubberDucksPage;
@@ -8,30 +10,40 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class TestClass extends TestBase{
     @Test
+    @Feature("SP-01")
+    @Description("Home page title test")
     public void homePageTest() {
         HomePage.homePageButtonClick();
         $("title").shouldHave(attribute("text", "My Store | Online Store"));
     }
 
     @Test
+    @Feature("SP-02")
+    @Description("Rubber ducks page title test")
     public void rubberDucksPageTest() {
         HomePage.rubberDucksPageButtonClick();
         $("title").shouldHave(attribute("text", "Rubber Ducks | My Store"));
     }
 
     @Test
+    @Feature("SP-03")
+    @Description("Delivery information page title test")
     public void deliveryInformationPageTest() {
         HomePage.deliveryInformationPageLinkClick();
         $("title").shouldHave(attribute("text", "Delivery Information | Customer Service | My Store"));
     }
 
     @Test
+    @Feature("SP-04")
+    @Description("Terms and conditions page title test")
     public void termsAndConditionsTest() {
         HomePage.termsAndConditionsLinkClick();
         $("title").shouldHave(attribute("text", "Terms of Purchase | Customer Service | My Store"));
     }
 
     @Test
+    @Feature("SP-05")
+    @Description("Sort ducks by name test")
     public void sortByNameTest() {
         HomePage.rubberDucksPageButtonClick();
         RubberDucksPage.sortByNameButtonClick();
@@ -47,6 +59,8 @@ public class TestClass extends TestBase{
     }
 
     @Test
+    @Feature("SP-06")
+    @Description("Sort ducks by price test")
     public void sortByPriceTest() {
         HomePage.rubberDucksPageButtonClick();
         $(".name").shouldBe(visible);
@@ -61,18 +75,24 @@ public class TestClass extends TestBase{
     }
 
     @Test
+    @Feature("SP-07")
+    @Description("Green duck new label test")
     public void greenDuckLabelTest() {
         HomePage.rubberDucksPageButtonClick();
         $(RubberDucksPage.greenDuckLabelText()).shouldHave(text("new"));
     }
 
     @Test
+    @Feature("SP-08")
+    @Description("Yellow duck sale label test")
     public void yellowDuckLabelTest() {
         HomePage.rubberDucksPageButtonClick();
         $(RubberDucksPage.yellowDuckLabelText()).shouldHave(text("sale"));
     }
 
     @Test
+    @Feature("SP-09")
+    @Description("LiteCart page title test")
     public void openLiteCartSiteTest() {
         HomePage.liteCartLinkClick();
 
